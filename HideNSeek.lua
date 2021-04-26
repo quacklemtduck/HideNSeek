b3af557655bf0374b106d3383b77cedd09af02fa
QuacklemtDuck <andreas.just.petersen@gmail.com>
2021-04-26 14:05:21 +0200

    Added a button for starting games and added a counter to keep track of how many times a player has been found

--- HideNSeek.lua
+++ HideNSeek.lua
@@ -7,7 +7,7 @@ UI:RegisterForDrag("LeftButton");
 UI:SetScript("OnDragStart", UI.StartMoving);
 UI:SetScript("OnDragStop", UI.StopMovingOrSizing);
 HnS_WIDTH = 270;
-HnS_HEIGHT = 120;
+HnS_HEIGHT = 145;
 UI:SetSize(HnS_WIDTH, HnS_HEIGHT);
 UI.Title:SetText("Hide N Seek");
 UI:SetShown(false);
@@ -16,10 +16,13 @@ UI:SetClampedToScreen(true);
 UI:RegisterEvent("PLAYER_TARGET_CHANGED");
 
 UI:SetScript('OnEvent', function(self, event, arg1, arg2, ...)
-    HnS_findTarget();
+    if HnS_playing then
+        HnS_findTarget();
+    end
 end
 );
 
+HnS_playing = false;
 
 local xPoint = 0;
 local yPoint = 0;
@@ -30,7 +33,7 @@ local yPoint = 0;
 addPlayerButton = CreateFrame("Button", "addPlayerButton", UI, "UIPanelButtonTemplate");
 addPlayerButton:SetText("Add Player");
 addPlayerButton:SetWidth(addPlayerButton:GetTextWidth() + 20);
-addPlayerButton:SetPoint("BOTTOMRIGHT", UI,"BOTTOMRIGHT", -10,  10);
+addPlayerButton:SetPoint("BOTTOMRIGHT", UI,"BOTTOMRIGHT", -10,  35);
 addPlayerButton:SetScript("OnClick", function ()
     HnS_addTarget();
 end
@@ -49,17 +52,31 @@ end
 resetButton = CreateFrame("Button", "resetButton", UI, "UIPanelButtonTemplate");
 resetButton:SetText("Reset Found Status");
 resetButton:SetWidth(resetButton:GetTextWidth() + 20);
-resetButton:SetPoint("BOTTOMLEFT", UI, "BOTTOMLEFT", 10, 10)
+resetButton:SetPoint("BOTTOMLEFT", UI, "BOTTOMLEFT", 10, 35)
 resetButton:SetScript("OnClick", function ()
     HnS_clearFind()
     HnS_updateText()
 end
 );
 
-statsButton = CreateFrame("Button", "statsButton", UI, "UIPanelButtonTemplate");
-statsButton:SetText("Stats");
-statsButton:SetWidth(statsButton:GetTextWidth() + 20);
-statsButton:SetPoint("TOPRIGHT", UI, "TOPRIGHT", -10, -30);
+startButton = CreateFrame("Button", "startButton", UI, "UIPanelButtonTemplate");
+startButton:SetText("Start Game");
+startButton:SetPoint("BOTTOMRIGHT", UI, "BOTTOMRIGHT", -10, 10);
+startButton:SetPoint("BOTTOMLEFT", UI, "BOTTOMLEFT", 10, 10);
+startButton:SetScript("OnClick", function ()
+    if(HnS_playing) then
+        HnS_playing = false;
+        startButton:SetText("Start Game");
+    else
+        HnS_playing = true;
+        startButton:SetText("Stop Game");
+    end
+end);
+
+exportButton = CreateFrame("Button", "exportButton", UI, "UIPanelButtonTemplate");
+exportButton:SetText("Export");
+exportButton:SetWidth(exportButton:GetTextWidth() + 20);
+exportButton:SetPoint("TOPRIGHT", UI, "TOPRIGHT", -10, -30);
 
 foundCounter = UI:CreateFontString("addPlayer", UI, "GameFontNormal");
 foundCounter:SetPoint("TOP", 0, -60);
@@ -82,7 +99,7 @@ function HnS_addTarget()
     end
 
     if(not HnS_has_value(HnS_Players, name)) then
-        table.insert(HnS_Players, {name = name, found = false});
+        table.insert(HnS_Players, {name = name, found = false, count = 0});
     end
     HnS_updateText()
 end
@@ -97,7 +114,10 @@ function HnS_findTarget()
     end
     for index, value in ipairs(HnS_Players) do
         if value.name == name then
-            value.found = true;
+            if not value.found then
+                value.found = true;
+                value.count = value.count +1;
+            end
         end
     end
     HnS_updateText();
@@ -115,7 +135,7 @@ function HnS_updateText()
     for i,n in ipairs(HnS_Players) do
         local f = HnS_getframe();
         table.insert(textinuse, f);
-        f:SetText(n.name);
+        f:SetText(n.name .. " : " .. tostring(n.count));
         if(n.found == true)then
             f:SetTextColor(0,1,0);
             HnS_found = HnS_found + 1;
